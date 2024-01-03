84eebdb1d39c1ca7f4898fe16500da3a9f0feeee
Antonino Lo Bianco <antonino.lobianco@outlook.com>
2024-01-03 15:00:32 +0100

    menu haburger

--- main.js
+++ main.js
@@ -51,6 +51,13 @@ const applylightTheme = () => {
     setTimeout(() => {
         const elements = document.querySelectorAll('*');
         const text = document.querySelectorAll('p , h1 , h2 , h3 , h4 , h5 , h6 , a , span , li , label , input , textarea , button , .word-container');
+        const body = document.querySelector('body');
+        const modal = document.querySelector('.modal');
+        const about = document.querySelector('.about-li-modal');
+        const work = document.querySelector('.work-li-modal');
+        const cross = document.querySelector('.cross');
+        const line = document.querySelector('.line');
+        const copyright = document.querySelector('.copyright-modal');
         elements.forEach(element => {
             const backgroundColor = window.getComputedStyle(element).backgroundColor;
             if (backgroundColor === 'rgb(7, 11, 28)') {
@@ -63,8 +70,17 @@ const applylightTheme = () => {
                 element.style.color = '#121c46';
             }
         });
+        body.style.backgroundColor = 'rgb(223, 223, 223)';
         btnMoon.style.display = 'none';
         btnSun.style.display = 'block';
+
+        modal.style.backgroundColor = 'rgb(18, 28, 70)'
+        about.style.color = '#ffffff';
+        work.style.color = '#ffffff';
+        cross.style.color = '#ffffff';
+        line.style.backgroundColor = '#ffffff';
+        copyright.style.color = '#ffffff'
+
         localStorage.setItem('theme', 'light');
     }, 1001);
 };
@@ -73,6 +89,13 @@ const applyDarkTheme = () => {
     setTimeout(() => {
         const elements = document.querySelectorAll('*');
         const text = document.querySelectorAll('p , h1 , h2 , h3 , h4 , h5 , h6 , a , span , li , label , input , textarea , button , .word-container');
+        const body = document.querySelector('body');
+        const modal = document.querySelector('.modal');
+        const about = document.querySelector('.about-li-modal');
+        const work = document.querySelector('.work-li-modal');
+        const cross = document.querySelector('.cross');
+        const line = document.querySelector('.line');
+        const copyrigth = document.querySelector('.copyright-modal');
         elements.forEach(element => {
             const backgroundColor = window.getComputedStyle(element).backgroundColor;
             if (backgroundColor === 'rgb(255, 255, 255)' || backgroundColor === 'rgba(255, 255, 255, 0)') {
@@ -85,9 +108,21 @@ const applyDarkTheme = () => {
                 element.style.color = '#ffffff';
             }
         });
-
+        body.style.backgroundColor = '#08051cd6'
         btnMoon.style.display = 'block';
         btnSun.style.display = 'none';
+
+        modal.style.backgroundColor = 'rgba(255, 255, 255, 0.99)';
+        about.style.color = 'rgb(18, 28, 70)';
+        work.style.color = 'rgb(18, 28, 70)';
+        copyrigth.style.color = 'rgb(18, 28, 70)';
+        btnMoon.style.display = 'block';
+        btnSun.style.display = 'none';
+        about.style.color = 'rgb(18, 28, 70)';
+        work.style.color = 'rgb(18, 28, 70)';
+        cross.style.color = 'rgb(18, 28, 70)';
+        line.style.backgroundColor = 'rgb(18, 28, 70)';
+
 
         localStorage.setItem('theme', 'dark');
     }, 1001);
@@ -103,7 +138,14 @@ btnMoon.addEventListener('click', () => {
 const setLightTheme = () => {
     const elements = document.querySelectorAll('*');
     const text = document.querySelectorAll('p , h1 , h2 , h3 , h4 , h5 , h6 , a , span , li , label , input , textarea , button , .word-container');
-
+    const body = document.querySelector('body');
+    const hamburger = document.querySelector('.hamburger')
+    const modal = document.querySelector('.modal');
+    const about = document.querySelector('.about-li-modal');
+    const work = document.querySelector('.work-li-modal');
+    const cross = document.querySelector('.cross');
+    const line = document.querySelector('.line');
+    const copyright = document.querySelector('.copyright-modal')
     elements.forEach(element => {
         const backgroundColor = window.getComputedStyle(element).backgroundColor;
         if (backgroundColor === 'rgb(7, 11, 28)') {
@@ -116,13 +158,87 @@ const setLightTheme = () => {
             element.style.color = '#121c46';
         }
     });
+    body.style.backgroundColor = 'rgb(223, 223, 223)';
     btnMoon.style.display = 'none';
     btnSun.style.display = 'block';
+
+    modal.style.backgroundColor = 'rgb(18, 28, 70)'
+    about.style.color = '#ffffff';
+    work.style.color = '#ffffff';
+    cross.style.color = '#ffffff';
+    line.style.backgroundColor = '#ffffff';
+    copyright.style.color = '#ffffff'
 };
+const setDarkTheme = () => {
+    const elements = document.querySelectorAll('*');
+    const text = document.querySelectorAll('p , h1 , h2 , h3 , h4 , h5 , h6 , a , span , li , label , input , textarea , button , .word-container');
+    const body = document.querySelector('body');
+    const modal = document.querySelector('.modal');
+    const about = document.querySelector('.about-li-modal');
+    const work = document.querySelector('.work-li-modal');
+    const cross = document.querySelector('.cross');
+    const line = document.querySelector('.line');
+    const copyrigth = document.querySelector('.copyright-modal');
+    elements.forEach(element => {
+        const backgroundColor = window.getComputedStyle(element).backgroundColor;
+        if (backgroundColor === 'rgb(255, 255, 255)' || backgroundColor === 'rgba(255, 255, 255, 0)') {
+            element.style.backgroundColor = '#070b1c';
+        }
+    });
+    text.forEach(element => {
+        const color = window.getComputedStyle(element).color;
+        if (color === 'rgb(18, 28, 70)') {
+            element.style.color = '#ffffff';
+        }
+    });
+    body.style.backgroundColor = '#08051cd6';
+    modal.style.backgroundColor = 'rgba(255, 255, 255, 0.99)';
+    about.style.color = 'rgb(18, 28, 70)';
+    work.style.color = 'rgb(18, 28, 70)';
+    copyrigth.style.color = 'rgb(18, 28, 70)';
+    btnMoon.style.display = 'block';
+    btnSun.style.display = 'none';
+    about.style.color = 'rgb(18, 28, 70)';
+    work.style.color = 'rgb(18, 28, 70)';
+    cross.style.color = 'rgb(18, 28, 70)';
+    line.style.backgroundColor = 'rgb(18, 28, 70)';
+};
+
+function modal() {
+    const hamburger = document.querySelector('.hamburger');
+    const modal = document.querySelector('.modal');
+    const cross = document.querySelector('.cross');
+    const about = document.querySelector('.about-li-modal');
+    const work = document.querySelector('.work-li-modal');
+    hamburger.addEventListener('click', () => {
+        console.log('click on the button');
+        modal.style.transition = 'transform 700ms ease-in-out';
+        modal.style.transform = 'translateX(0%)';
+        cross.addEventListener('click', () => {
+            modal.style.transition = 'transform 700ms ease-in-out';
+            modal.style.transform = 'translateX(100%)';
+        });
+        about.addEventListener('click', () => {
+            setTimeout(() => {
+                modal.style.transform = 'translateX(100%)';
+            }, 1001);
+
+        });
+        work.addEventListener('click', () => {
+            setTimeout(() => {
+                modal.style.transition = 'transform 100ms ease-in-out';
+                modal.style.transform = 'translateX(100%)';
+            }, 1001);
+
+        });
+    });
+}
+modal();
+
 document.addEventListener('DOMContentLoaded', function () {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
-        applyDarkTheme();
+        setDarkTheme();
     } if (savedTheme === 'light') {
         setLightTheme();
     }
